d4f6aeb173dce696155780d0eb9884ee5ab5737c
lacajenne <giuseppe.lacagnina@gmail.com>
2018-03-21 18:01:43 +0100

    this is a first edit, removed useless stuff

--- src/test/java/firstSelenideTest/tests/FirstTest.java
+++ src/test/java/firstSelenideTest/tests/FirstTest.java
@@ -1,12 +1,10 @@
 package firstSelenideTest.tests;
 
-import com.codeborne.selenide.CollectionCondition;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FirstTest {
