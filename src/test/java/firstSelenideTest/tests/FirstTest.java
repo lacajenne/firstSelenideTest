6167b91d4057f9aa6bd543b345bd4002fa349fdf
lacajenne <giuseppe.lacagnina@gmail.com>
2018-03-22 08:51:36 +0000

    extending the test with elment identification via attribute

--- src/test/java/firstSelenideTest/tests/FirstTest.java
+++ src/test/java/firstSelenideTest/tests/FirstTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -23,7 +24,7 @@ public class FirstTest {
         // open Google
         open("http://www.google.com");
 
-        // search for "Selenide
+        // search for "Selenide"
         $(By.name("q")).setValue("Selenide").pressEnter();
 
         // check that the div with "All" exists
@@ -31,6 +32,9 @@ public class FirstTest {
 
         // click on the link of a search result
         $(By.linkText("Selenide: concise UI tests in Java")).click();
+
+        // find the Selenide logo by element attribute specification
+        $(byAttribute("src", "/images/selenide-logo-big.png")).should(exist);
     }
 
 }
