cf9abed59e9f1caac582ff009f5ea3ee64103ed8
lacajenne <giuseppe.lacagnina@gmail.com>
2018-03-22 08:31:35 +0000

    extended the test, added comments

--- src/test/java/firstSelenideTest/tests/FirstTest.java
+++ src/test/java/firstSelenideTest/tests/FirstTest.java
@@ -1,9 +1,11 @@
 package firstSelenideTest.tests;
 
+import com.codeborne.selenide.commands.Exists;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -18,8 +20,16 @@ public class FirstTest {
 
     @Test
     public void GoogleTest() {
+        // open Google
         open("http://www.google.com");
+
+        // search for "Selenide
         $(By.name("q")).setValue("Selenide").pressEnter();
+
+        // check that the div with "All" exists
+        $("#hdtb-msb-vis").should(exist);
+
+        // click on the link of a search result
         $(By.linkText("Selenide: concise UI tests in Java")).click();
     }
 
